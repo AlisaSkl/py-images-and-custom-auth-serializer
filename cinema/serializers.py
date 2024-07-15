e66ed537dc47b45eecdee5eeb1c7ae393e5c4272
Alisa <alisasklrv@gmail.com>
2024-07-15 17:17:30 +0300

    solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -85,9 +85,10 @@ class MovieDetailSerializer(MovieSerializer):
 
 
 class MovieSessionSerializer(serializers.ModelSerializer):
+    movie_image = serializers.ImageField(source="movie.image", read_only=True)
     class Meta:
         model = MovieSession
-        fields = ("id", "show_time", "movie", "cinema_hall")
+        fields = ("id", "show_time", "movie", "cinema_hall", "movie_image")
 
 
 class MovieSessionListSerializer(MovieSessionSerializer):
